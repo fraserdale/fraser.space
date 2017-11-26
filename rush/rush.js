064ad4407f31e81118368f785390456654a1ef0d
fraserdale <fraserdale2@gmail.com>
2017-11-26 17:38:54 +0000

    block draws
    Need to put h1 behind canvas i think

--- rush/rush.js
+++ rush/rush.js
@@ -2,34 +2,39 @@ var canvas = document.getElementById('rush');
 var cont = canvas.getContext('2d');
 cont.scale(20, 20);
 
-var shapes =
-    [
-        [1,1],
-        [1,1]
-    ];
+var shapes =[
+    [1,1,0],
+    [0,1,1],
+    [0,0,0]
+];
 
-//initialise
-function fill(){
-    cont.fillStyle ="#000";
+function fill() {
+    cont.fillStyle = '#232323';
     cont.fillRect(0,0,canvas.width,canvas.height);
+    draw(current.shape);
 }
 
+
+//initialise
 function draw(shape) {
-    for(var row = 0; row = shape.rows[row]; row++){
-        for(var index = 0; index = shape.cells[index]; index++){
-            if(index == 1){
+    for(var row = 0; row < shape.length; row++){
+        //console.log(row);
+        for(var index = 0; index < shape.length; index++){
+            console.log(shape[row][index]);
+            if(shape[row][index] == 1){
                 cont.fillStyle = "#FFEE00";
-                cont.fillRect(row,index,1,1)
+                cont.fillRect(index + 4 ,row+10,1,1)
             }
         }
     }
-
 }
 
-
-
 var current ={
-    shape: shapes[0]
+    shape: shapes
+
 };
 
-draw(current.shape)
+fill();
+//testing to make sure correct array is showing
+//console.log(current.shape[0]);
+//console.log(current.shape[0][0]);
