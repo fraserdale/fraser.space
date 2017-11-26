79e3ee35c7226827eb8d386f2824108741fedc7b
fraserdale <fraserdale2@gmail.com>
2017-11-26 18:59:44 +0000

    can move left right and up

--- rush/rush.js
+++ rush/rush.js
@@ -11,30 +11,52 @@ var shapes =[
 function fill() {
     cont.fillStyle = '#232323';
     cont.fillRect(0,0,canvas.width,canvas.height);
-    draw(current.shape);
+    draw(current.shape, current.position.x, current.position.y);
 }
 
 
 //initialise
-function draw(shape) {
+function draw(shape, posx, posy) {
     for(var row = 0; row < shape.length; row++){
         //console.log(row);
         for(var index = 0; index < shape.length; index++){
             console.log(shape[row][index]);
             if(shape[row][index] == 1){
                 cont.fillStyle = "#FFEE00";
-                cont.fillRect(index + 4 ,row+10,1,1)
+                cont.fillRect(index + posx ,row + posy,1,1)
             }
         }
     }
 }
 
 var current ={
-    shape: shapes
-
+    shape: shapes,
+    position: {
+        x: 5,
+        y: 0
+    }
 };
 
-fill();
+document.onkeypress = function(k){
+    if(k.keyCode == 97){
+        console.log("a");
+        current.position.x -= 1;
+        console.log(current.position);
+        draw(current.shape,current.position.x,current.position.y)}
+    else if(k.keyCode == 100){
+        console.log("d");
+        current.position.x += 1;
+        console.log(current.position);
+        draw(current.shape,current.position.x,current.position.y)}
+    else if(k.keyCode == 115){
+        console.log("s");
+        current.position.y += 1;
+        console.log(current.position);
+        draw(current.shape,current.position.x,current.position.y)}
+    else if(k.keyCode == 119){
+        console.log("w")}
+};
+fill()
 //testing to make sure correct array is showing
 //console.log(current.shape[0]);
 //console.log(current.shape[0][0]);
