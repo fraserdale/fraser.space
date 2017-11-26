66df1d0e744103cd4c74bd80beb58bbeac4c0db4
fraserdale <fraserdale2@gmail.com>
2017-11-26 16:08:59 +0000

    fill canvas

--- rush/rush.js
+++ rush/rush.js
@@ -1,3 +1,9 @@
 var canvas = document.getElementById('rush');
-var context = canvas.getContext('2d');
-context.scale(20, 20)
+var cont = canvas.getContext('2d');
+cont.scale(20, 20);
+
+//initialise
+function fill(){
+    cont.fillStyle ="#000";
+    cont.fillRect(0,0,canvas.width,canvas.height);
+}
