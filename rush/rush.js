6a9875c4df2c628f1e08b48b8570581af03e35e0
fraserdale <fraserdale2@gmail.com>
2018-03-05 17:22:50 +0000

    fds

--- rush/rush.js
+++ rush/rush.js
@@ -1,7 +1,11 @@
+//declare canvas and link with page
 var canvas = document.getElementById('rush');
 var cont = canvas.getContext('2d');
+//scale by 20x
 cont.scale(20, 20);
+//declare the colours that correspond with the appropriate shapes
 var colours = ['orange','yellow','red','lime','blue','purple']
+//declare the shapes and their rotations
 var shapes =[
     [
         [0,0,0],
@@ -133,9 +137,11 @@ var shapes =[
 
 ];
 
+//set rotation counter and shape counter to 0
 var n = 0
 var counter = 0
 
+//declare the first shape that drops and its colour and its position x and y positions
 var current ={
     shape: shapes[n],
     colour: colours[0],
@@ -145,41 +151,54 @@ var current ={
     },
 };
 
+//reads in stored cookie and splits from the = sign, this seperates the name of the cookie and the data
 var totalCookie = document.cookie.split("=");
 var scoresCookie = totalCookie[1];
+//splits the stored scores cookie into an array
 var scores = scoresCookie.split(",");
-
+//turns the scores into integers from strings
 for(var e=0; e<scores.length; e++){
     scores[e] = parseInt(scores[e])
 }
 
+//initialises score as 0
 score = 0
 
+//creates the 'backTable' used to store all previous moves
 var backTable = [];
+//loops through each row
 for (var x = 0; x < 20; x++){
     var inner = [];
+    //loops through each collumn
     for (var y = 0; y < 12; y++){
+        //fills each cell with a 0, a blank canvas
         inner[y] = 0;
     }
     backTable[x] = inner;
 }
 
+//function to fill the canvas
 function fill() {
+    //colours the canvas content to match rest of the screen
     cont.fillStyle = '#232323';
+    //sets canvas content to same size as canvas
     cont.fillRect(0,0,canvas.width,canvas.height);
+    //draws the current tile
     draw(current.shape, current.position.x, current.position.y);
+    //draws the back table, all previously placed tiles
     drawGrid(backTable)
 
 }
 
 
-//initialise
 function draw(shape, posx, posy) {
+    //for each row of the shape
     for(var row = 0; row < shape.length; row++){
-
+        //for each collumn in that row
         for(var index = 0; index < shape.length; index++){
-
+            //if that location is filled (equals one)
             if(shape[row][index] == 1){
+                //on the canvas content draw a 1x1 square of the current colour
                 cont.fillStyle = current.colour;
                 cont.fillRect(index + posx ,row + posy,1,1)
             }
@@ -187,15 +206,17 @@ function draw(shape, posx, posy) {
     }
 }
 
+//function to draw a shape to the back table
 function drawBack(shape, posx, posy, backTable){
-
+    //for each row of the shape
     for(var row = 0; row < shape.length; row++){
-
+        //for each collumn in that row
         for(var index = 0; index < shape.length; index++){
-
+            //if that location is filled (equals 1)
             if(shape[row][index] == 1){
-                //console.log(backTable[row + posy][index + posx])
+                //draw that position to the backboard
                 backTable[row + posy][index + posx] = 1
+                //increase the score by 1
                 score += 1
             }
         }
@@ -203,43 +224,57 @@ function drawBack(shape, posx, posy, backTable){
 }
 
 
-
+//draws the entire back table
 function drawGrid(backTable) {
+    //for each row in the backtable
     for (var y = 0; y < 20; y++) {
+        //for each item in the row
         for (var x = 0; x < 12; x++) {
+            //if in that location onthe back table = 1
             if (backTable[y][x] == 1) {
+                //on the canvas content draw a 1x1 square of the current colour
                 cont.fillStyle = current.colour;
                 cont.fillRect(x,y, 1, 1)
             }
 
         }
     }
+    //Fill the score in the side bar
     document.getElementById("score").innerHTML = "Score: " + score
 }
 
 
-
+//drop counter that ticks every 1000ms/ every second
 var interval = setInterval(drop,1000);
-var n = 0
 
+//function to drop tile down
 function drop() {
     fill()
+    //checks if game should be over by checking the top row to see if filled
     if(backTable[0][6] == 1 || backTable[0][5] == 1 || backTable[0][7] == 1){
-        alert("GAME OVER")
+        //if game is over then sort the scores
         sortScores()
+        //stop the drop clock
         clearInterval(interval)
+        console.log("Game over")
+        //redirect to the scores page
         window.open("scores.html","_self")
     }
+    //checks if tile is on the bottom row
     if(current.position.y - checkBottom() == 17){
+        //if it is then call collision function
         collision()
     }
     shape = current.shape
     posy = current.position.y
     posx = current.position.x
+    //loop through each row in the shape
     for(var row = 0; row < shape.length; row++){
-
+        //loop through each element in the row
         for(var index = 0; index < shape.length; index++){
+            //if that element equals 1
             if(shape[row][index] == 1){
+
                 cont.fillStyle = current.colour;
                 //console.log(row + posy + 1 + " " + (index + posx) )
                 if(backTable[row + posy + 1][index + posx] == 1)
@@ -360,50 +395,37 @@ function checkBottom(){
 }
 
 
-document.onkeypress = function(k){
-    if(k.keyCode == 97){
-        //console.log("a");
-        if (checkLHS()){
+document.onkeypress = function(key) {
+    if (key.keyCode == 97) {
+        if (checkLHS()) {
             current.position.x -= 1;
-        };
-        //current.position.x -= 1;
-        //console.log(current.position);
-        fill();}
-    else if(k.keyCode == 100){
-        //console.log("d");
-        if (checkRHS()){
+        }
+        ;
+        fill();
+    }
+    else if (key.keyCode == 100) {
+        if (checkRHS()) {
             current.position.x += 1;
-        };
-        //current.position.x += 1;
-        //console.log(current.position);
-        fill()}
-    else if(k.keyCode == 115){
-        //console.log("s");
-            drop()
-        //console.log(current.position);
-        fill();}
-    else if(k.keyCode == 119) {
+        }
+        ;
+        fill()
+    }
+    else if (key.keyCode == 115) {
+        drop()
+        fill();
+    }
+    else if (key.keyCode == 119) {
         if (counter < 3) {
             n++;
             counter++
-        }else {
+        } else {
             counter = 0;
             n -= 3
         }
-    current.shape = shapes[n]
-
-
-    fill()
-    console.log("n is = " + n)
-        console.log("counter is = " + counter)}
+        current.shape = shapes[n]
+        fill()
+    };
 };
 fill();
-
-
-
-
-//testing to make sure correct array is showin
-//console.log(current.shape[0])
-//console.log(current.shape[0][0])
 
 
